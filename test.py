b0e1f1da7c4dfc929afd678015e5624e68078b0a
Nicolás Quinteros <nicobquinteros@gmail.com>
2025-09-05 16:35:39 -0300

    despliegue de columnas del df

--- test.py
+++ test.py
@@ -5,3 +5,13 @@ import seaborn as sns
 
 print("✅ Todo instalado correctamente")
 print("Versión de pandas:", pd.__version__)
+
+# Configuración visual
+plt.style.use("seaborn-v0_8")
+sns.set_palette("pastel")
+
+# Carga de dataset
+df = pd.read_csv("DataCoSupplyChainDataset.csv", encoding="latin1")
+
+print("\nColumnas disponibles en el dataset:")
+print(df.columns.tolist())
